8cc744a2a8359cdcf096d19fb1f4c8fb595b3f57
Pikatsuto <pikatsuto@gmail.com>
2022-07-28 12:58:26 +0200

    Recoding to 2.0.0

--- site/index.js
+++ site/index.js
@@ -1,111 +1,122 @@
-function documentIsReady(frameObject) {
-    alert(frameObject.contentDocument.body.innerHTML);
-}
-
 function updatemenu() {
     if (document.getElementById('responsive-menu').checked == true) {
-      document.getElementById('menu').style.borderBottomRightRadius = '0';
-      document.getElementById('menu').style.borderBottomLeftRadius = '0';
+        document.getElementById('menu').style.borderBottomRightRadius = '0';
+        document.getElementById('menu').style.borderBottomLeftRadius = '0';
     }else{
-      document.getElementById('menu').style.borderRadius = '10px';
+        document.getElementById('menu').style.borderRadius = '10px';
     }
-  }
+}
 
-  var showTogg = 0
-  var execTogg = []
-  var numTogg = 0
+var showTogg = 0
+var execTogg = []
+var execTogg2 = []
+var numTogg = 0
 
 function onOffTogg(showBoxId, showBoxId1, maskBoxId, maskBoxId1, buttun) {
-if (showTogg === 0) {
-    multiTogg(maskBoxId, maskBoxId1, false);
-    showTogg ++
-    document.getElementById(buttun).classList.add("is-show");
-}
-else if (showTogg === 1){
-    multiTogg(maskBoxId, maskBoxId1, false);
-    multiTogg(showBoxId, showBoxId1, false);
-    showTogg ++
-}
-else if (showTogg >= 2){
-    multiTogg(showBoxId, showBoxId1, false);
-    showTogg = 0
-    document.getElementById(buttun).classList.remove("is-show");
-};
+    if (showTogg === 0) {
+        multiTogg(maskBoxId, maskBoxId1, false);
+        showTogg ++
+        document.getElementById(buttun).classList.add("is-show");
+    }
+    else if (showTogg === 1){
+        multiTogg(maskBoxId, maskBoxId1, false);
+        multiTogg(showBoxId, showBoxId1, false);
+        showTogg ++
+    }
+    else if (showTogg >= 2){
+        multiTogg(showBoxId, showBoxId1, false);
+        showTogg = 0
+        document.getElementById(buttun).classList.remove("is-show");
+    };
 };
 
 function togg(showBoxId){
-            
-for (let myClass of document.getElementsByClassName("showBoxClass")) {
-    myClass.style.display = 'none';
-}
 
-document.getElementById(showBoxId).style.display = 'flex';
+    for (let myClass of document.getElementsByClassName("showBoxClass")) {
+        myClass.style.display = 'none';
+    }
+
+    document.getElementById(showBoxId).style.display = 'flex';
 };
+
+function offToggAll(){
+
+    for (let id of execTogg2) {
+        multiTogg(id, "site", true)
+    }
+    execTogg2 = []
+    numTogg = 0
+};
+
 function multiTogg(showBoxId, showBoxId1, ifButtun){
 
-togg(showBoxId1)
+    togg(showBoxId1)
 
-var isTogg = document.getElementById(showBoxId).style.display
+    var isTogg = document.getElementById(showBoxId).style.display
 
-for (let myClass of document.getElementsByClassName("showClass")) {
-    myClass.classList.remove("iframeFois4");
-    myClass.classList.remove("iframeFois3");
-    myClass.classList.remove("iframeFois2");
-    myClass.classList.remove("inShowBoxClass");
-}
+    for (let myClass of document.getElementsByClassName("showClass")) {
+        myClass.classList.remove("iframeFois4");
+        myClass.classList.remove("iframeFois3");
+        myClass.classList.remove("iframeFois2");
+        myClass.classList.remove("inShowBoxClass");
+    }
 
-if(isTogg == 'none'){
-    if (numTogg < 4) {
-    onTogg(showBoxId);
-    numTogg ++
-    if (ifButtun) {
-        document.getElementById(showBoxId + "Menu").classList.add("is-show");
-    }
-    }
-}
-else {          
-    if (execTogg.indexOf(showBoxId) !== -1) {
-    offTogg(showBoxId);
-    numTogg --
-    if (ifButtun) {
-        document.getElementById(showBoxId + "Menu").classList.remove("is-show");
-    }
+    if(isTogg == 'none'){
+        if (numTogg < 4) {
+            onTogg(showBoxId);
+            numTogg ++
+            execTogg2.push(showBoxId);
+            if (ifButtun) {
+                document.getElementById(showBoxId + "Menu").classList.add("is-show");
+            }
+        }
     }
     else {
-    if (numTogg < 4) {
-        onTogg(showBoxId);
-        execTogg.push(showBoxId);
-        numTogg ++
-        if (ifButtun) {
-        document.getElementById(showBoxId + "Menu").classList.add("is-show");
+        if (execTogg.indexOf(showBoxId) !== -1) {
+            offTogg(showBoxId);
+            numTogg --
+            let index = execTogg2.indexOf(showBoxId);
+            execTogg.splice(index, 1);
+            if (ifButtun) {
+                document.getElementById(showBoxId + "Menu").classList.remove("is-show");
+            }
         }
+        else {
+            if (numTogg < 4) {
+                onTogg(showBoxId);
+                execTogg.push(showBoxId);
+                execTogg2.push(showBoxId);
+                numTogg ++
+                if (ifButtun) {
+                    document.getElementById(showBoxId + "Menu").classList.add("is-show");
+                }
+            };
+        };
     };
-    };
-};
 
-if (numTogg == 1){
-    for (let myClass of document.getElementsByClassName("showClass")) {
-    myClass.classList.add("inShowBoxClass");
+    if (numTogg == 1){
+        for (let myClass of document.getElementsByClassName("showClass")) {
+            myClass.classList.add("inShowBoxClass");
+        }
+    } else if (numTogg == 2){
+        for (let myClass of document.getElementsByClassName("showClass")) {
+            myClass.classList.add("iframeFois2");
+        }
+    } else if (numTogg == 3){
+        for (let myClass of document.getElementsByClassName("showClass")) {
+            myClass.classList.add("iframeFois3");
+        }
+    } else if (numTogg == 4){
+        for (let myClass of document.getElementsByClassName("showClass")) {
+            myClass.classList.add("iframeFois4");
+        }
     }
-} else if (numTogg == 2){
-    for (let myClass of document.getElementsByClassName("showClass")) {
-    myClass.classList.add("iframeFois2");
-    }
-} else if (numTogg == 3){
-    for (let myClass of document.getElementsByClassName("showClass")) {
-    myClass.classList.add("iframeFois3");
-    }
-} else if (numTogg == 4){
-    for (let myClass of document.getElementsByClassName("showClass")) {
-    myClass.classList.add("iframeFois4");
-    }
-}
 };
 
 function onTogg(showBoxId){
-document.getElementById(showBoxId).style.display = 'flex';
+    document.getElementById(showBoxId).style.display = 'flex';
 };
 
 function offTogg(showBoxId){
-document.getElementById(showBoxId).style.display = 'none';
+    document.getElementById(showBoxId).style.display = 'none';
 };
